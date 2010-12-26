ca903b6b3e91b509af7f85148190d24b4b080d41
Jason Bernardino Alonso <jalonso@hackorp.com>
2010-12-25 20:05:23 -0500

    Introducing the ConnectionFactory class

---
+++ stomp-server/connectionfactory.js
@@ -0,0 +1,89 @@
+
+// stomp-server/connectionfactory.js -- Definition of the ConnectionFactory
+// class
+
+// Load modules
+var events      = require('events'),
+    Connection  = require('./connection').Connection;
+
+// Middleware structure: [{cbk: function(frame), ebk: function(frame)}...]
+// recv_middleware 
+// send_middleware
+function ConnectionFactory(bufferLimit) {
+    var self = this;
+
+    // Set the buffer limit
+    if( bufferLimit == null )
+        bufferLimit = 65536;
+
+    // Initialize members
+    this.bufferLimit = bufferLimit;
+    this.recv_middleware = [];
+    this.send_middleware = [];
+
+    // Extend EventEmitter
+    events.EventEmitter.call(this);
+
+    // Prepare the factory closure
+    this.newConnection = function(stream) {
+        return self._newConnection(stream);
+    };
+}
+
+ConnectionFactory.prototype._newConnection = function() {
+    var self = this;
+
+    // Construct a new Connection object
+    var conn = new Connection(stream, this.bufferLimit);
+
+    // Configure event handlers
+    conn.on('connect', function() { self.emit('connect', conn); });
+    conn.on('secure', function() { self.emit('secure', conn); });
+    conn.on('timeout', function() { self.emit('timeout', conn); });
+    conn.on('close', function(had_error) { self.emit('close', conn, had_error); });
+    conn.on('error', function(err) { self.emit('error', conn, err); });
+    conn.on('frame', function(frame_obj) {
+            // Process frame for reception through middleware
+            var layer, error_obj;
+            for( layer = 0; layer < this.recv_middleware.length; layer++ ) {
+                try {
+                    if( frame_obj && this.recv_middleware[layer].cbk )
+                        frame_obj = this.recv_middleware[layer].cbk(conn, frame_obj);
+                    else if( error_obj && this.recv_middleware[layer].ebk )
+                        frame_obj = this.recv_middleware[layer].ebk(conn, error_obj);
+                    error_obj = null;
+                } catch( err ) {
+                    error_obj = err;
+                    frame_obj = null;
+                }
+            }
+
+            // Handle remains
+            if( frame_obj ) this.emit('frame', conn, frame_obj);
+            else if( error_obj ) this.emit('error', error_obj);
+        });
+};
+
+ConnectionFactory.prototype.send_frame = function(conn, frame_obj) {
+    // Process frame for transmission through middleware
+    var layer, error_obj;
+    for( layer = 0; layer < this.send_middleware.length; layer++ ) {
+        try {
+            if( frame_obj && this.send_middleware[layer].cbk )
+                frame_obj = this.send_middleware[layer].cbk(conn, frame_obj);
+            else if( error_obj && this.send_middleware[layer].ebk )
+                frame_obj = this.send_middleware[layer].ebk(conn, error_obj);
+        } catch( err ) {
+            error_obj = err;
+            frame_obj = null;
+        }
+    }
+
+    // Transmit the frame
+    if( frame_obj ) conn.send(frame_obj);
+
+    // Operation Complete!
+};
+
+// Export classes
+module.exports.ConnectionFactory = ConnectionFactory;
