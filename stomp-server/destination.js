ca3d3b7f4ce9a730907cbfc317ec822a254decaf
Jason Bernardino Alonso <jalonso@media.mit.edu>
2011-03-19 14:28:25 -0400

    Boilerplate file for work on Destination classes

---
+++ stomp-server/destination.js
@@ -0,0 +1,27 @@
+
+// stomp-server/destination.js -- Definition of the Destination classes
+
+/*  Copyright 2010, Jason B. Alonso
+ *
+ *  This file is part of node-stomp-server.
+ *  
+ *  node-stomp-server is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  node-stomp-server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with node-stomp-server.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+// Load modules
+var sys             = require('sys');
+
+function AbstractDestination(name) {
+    var self = this;
+}
